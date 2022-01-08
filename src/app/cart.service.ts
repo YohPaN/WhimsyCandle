675d8a26372a6c513003e5fd6f05665f9042dbc4
YohPaN <yohanpaya74@hotmail.fr>
2022-01-08 12:18:48 +0100

    'commit of every component

---
+++ src/app/cart.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './products';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  items:Product[]=[];
+
+  addToCart(product:Product){
+    this.items.push(product);
+  }
+
+  getItems(){
+    return this.items;
+  }
+
+  clearCart(){
+    this.items = [];
+    return this.items;
+  }
+
+  getCollectionData(){
+    return this.http.get<{name: string, items:{itemName: string, price: number}[]}[]>('/assets/collection.json')
+  }
+  
+  constructor(
+    private http: HttpClient
+  ) { }
+}
